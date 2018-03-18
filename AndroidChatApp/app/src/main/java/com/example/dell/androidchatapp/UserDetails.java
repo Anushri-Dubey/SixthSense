2b590f583e4a0e3cd13a18d99568ca09e1d6ea49
Anushri Dubey <anushri1243@gmail.com>
2018-03-18 11:25:49 +0530

    Second Commit

---
+++ AndroidChatApp/app/src/main/java/com/example/dell/androidchatapp/UserDetails.java
@@ -0,0 +1,13 @@
+package com.example.dell.androidchatapp;
+
+/**
+ * Created by dell on 17/3/18.
+ */
+
+
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
